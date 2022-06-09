9570ec16f149264a349485c3e10a7a540947b68f
OnlyDuncan <Duncan.C.Payne@gmail.com>
2022-06-09 16:36:05 -0500

    Added my project

---
+++ data/misc/django_init.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS portfolioapp_db;
+CREATE DATABASE portfolioapp_db;
+
+\c portfolioapp_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_vodies = false;
+SET client_min_messages = warning;
+SET default_tablespace = '';
+SET default_with_oids = false;
